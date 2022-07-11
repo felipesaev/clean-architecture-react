7d9ebf916b5b00ac01b5173256ac3e2073d4adf2
felipecosta <felipesaev@gmail.com>
2022-07-11 08:41:16 -0300

    chore: test in nvim

---
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -0,0 +1,26 @@
+export function foot(a: number) {
+	return a + 1
+}
+
+foot()
+
+
+class RemoteAuthentication {
+  constructor(private readonly url: string) {
+
+  }
+
+  async auth(): Promise<void>{
+    return Promise.resolve()
+  }
+}
+
+describe('RemoteAuthentication', () => {
+  it('Should call HttpClient with correct URL', async () => {
+    const url = 'any_url'
+    const httpClient = HttpClient()
+      const sut = new RemoteAuthentication(url)
+      await sut.auth()
+      expect(httpClient.url).toBe(url)
+  })
+}) 
