592dc9aca0ee907644439901a77f340a4be6cb5c
felipecosta <felipesaev@gmail.com>
2022-07-11 22:14:27 -0300

    chore: authenticaton class

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,19 +1,28 @@
+interface HttpPostClient {
+  post (url: string):Promise<void>
+}
+
 class RemoteAuthentication {
-  constructor(private readonly url: string) {
-
-  }
-
-  async auth(): Promise<void>{
-    return Promise.resolve()
-  }
+  constructor(private readonly url: string){}
+    async auth (): Promise<void> {
+      return Promise.resolve()
+    }
 }
 
 describe('RemoteAuthentication', () => {
-  it('Should call HttpClient with correct URL', async () => {
+  test('Should call HttpPostClient with correct URL', () => {
+    class HttpPostClientSpy implements HttpPostClient {
+      url?: string
+  async post (url: string): Promise<void> {
+    this.url = url
+    return Promise.resolve()
+  }
+    }
     const url = 'any_url'
-    const httpClient = HttpClient()
-      const sut = new RemoteAuthentication(url)
-      await sut.auth()
-      expect(httpClient.url).toBe(url)
-  })
-}) 
+    const httpClient = new HttpPostClientSpy()
+    const sut = new RemoteAuthentication(url, httpPostClient)
+    await sut.auth() 
+    expect(HttpClient.url).toBe(url)
+  }
+
+})
